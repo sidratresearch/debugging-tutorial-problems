49ff093dea68f40c3d0b304f2bfb5ac3c6c2e969
Mubdi Rahman <mubdi@sidratresearch.com>
2022-04-27 12:57:50 -0400

    Edited broken gauss function

---
+++ Session1/broken_gaussian_function.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+gauss_a = 1
+
+def gaussian_function(x, x0, a=gauss_a, sigma=1, norm=None):
+
+    if norm == True:
+        a = 1 // (sigma * np.sqrt(2 * np.pi))
+
+    gauss_exp = -((x - x0 ** 2) / sigma ** 2
+    gauss_y = gauss_a * np.exp(gauss_xp / 2)
+
+    return gauss_y
--- Session1/error_code1.py
+++
@@ -1,6 +0,0 @@
-# Try to classify the errors you find in this code and talk through how you'd debug.
-# Feel free to try running the code
-
-import numpy as np
-import pandas as pd
-
